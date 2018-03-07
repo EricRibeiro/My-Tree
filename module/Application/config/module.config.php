db48a7756d46c886955aa936d7e6869b7fed2b3e
Eric Ribeiro <ericribeiro@outlook.com.br>
2018-03-07 11:11:34 -0300

    Fixed homepage route issue

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -2,6 +2,17 @@
 return array(
     'router' => array(
         'routes' => array(
+            'home' => array(
+                'type' => 'literal',
+                'options' => array(
+                    'route' => '/',
+                    'defaults' => array(
+                        '__NAMESPACE__' => 'Application\Controller',
+                        'controller' => 'Index',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
             'application' => array(
                 'type' => 'Segment',
                 'options' => array(
